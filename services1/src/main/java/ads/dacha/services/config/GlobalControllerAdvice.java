7cd3f76b202fc4910abd00fa6563b7a0656feee1
Glazkov58 <glazkov_pv58@gmail.com>
2025-09-25 06:18:49 +0400

    футер и хедер

---
+++ services1/src/main/java/ads/dacha/services/config/GlobalControllerAdvice.java
@@ -0,0 +1,16 @@
+package ads.dacha.services.config;
+
+import ads.dacha.services.models.User;
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ModelAttribute("currentUser")
+    public User addCurrentUser(HttpSession session) {
+        return (User) session.getAttribute("currentUser");
+    }
+}
